cd77f25951f7f227e3231ac86a7ff422fd06102f
wb66715 <wubo71566@gmail.com>
2025-11-07 22:46:24 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,125 @@
+import sys
+import math
+from tqdm import tqdm
+from FocalLoss import FocalLoss1 as FocalLoss
+import torch
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
+    model.train()
+    loss_function = FocalLoss()
+
+    accu_loss = torch.zeros(1).to(device)
+    accu_num = torch.zeros(1).to(device)
+    optimizer.zero_grad()
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images1, images2, labels = data
+        sample_num += images1.shape[0]
+
+        pred, out1, out2, align_loss = model(images1.to(device), images2.to(device))
+
+        pred_classes = torch.max(pred, dim=1)[1]
+
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device)) + loss_function(out1, labels.to(device)) + loss_function(out2,labels.to(device)) + align_loss
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+            optimizer.param_groups[0]["lr"]
+        )
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+        lr_scheduler.step()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = FocalLoss()
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)
+    accu_loss = torch.zeros(1).to(device)
+
+    sample_num = 0
+
+
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images1, images2, labels = data
+        sample_num += images1.shape[0]
+
+        pred, out1, out2, align_loss = model(images1.to(device), images2.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device)) + loss_function(out1, labels.to(device)) + loss_function(out2,labels.to(device)) + align_loss
+
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num
+        )
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+def create_lr_scheduler(optimizer,
+                        num_step: int,
+                        epochs: int,
+                        warmup=True,
+                        warmup_epochs=1,
+                        warmup_factor=1e-3,
+                        end_factor=1e-2):
+    assert num_step > 0 and epochs > 0
+    if warmup is False:
+        warmup_epochs = 0
+
+    def f(x):
+        if warmup is True and x <= (warmup_epochs * num_step):
+            alpha = float(x) / (warmup_epochs * num_step)
+            return warmup_factor * (1 - alpha) + alpha
+        else:
+            current_step = (x - warmup_epochs * num_step)
+            cosine_steps = (epochs - warmup_epochs) * num_step
+            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (1 - end_factor) + end_factor
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=f)
+
+
+def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
+    parameter_group_vars = {"decay": {"params": [], "weight_decay": weight_decay},
+                            "no_decay": {"params": [], "weight_decay": 0.}}
+
+    parameter_group_names = {"decay": {"params": [], "weight_decay": weight_decay},
+                             "no_decay": {"params": [], "weight_decay": 0.}}
+
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+
+        if len(param.shape) == 1 or name.endswith(".bias"):
+            group_name = "no_decay"
+        else:
+            group_name = "decay"
+
+        parameter_group_vars[group_name]["params"].append(param)
+        parameter_group_names[group_name]["params"].append(name)
+
+    # print("Param groups = %s" % json.dumps(parameter_group_names, indent=2))
+    return list(parameter_group_vars.values())
